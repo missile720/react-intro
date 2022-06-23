768fc120a6fccf803bdbb8cd8d480021e5d71649
Misael Bolainez <misaelbolainez@gmail.com>
2022-06-23 13:19:24 -0700

    feat(validation): added logic to verify input fields are not empty

--- react-intro/src/components/form/Form.js
+++ react-intro/src/components/form/Form.js
@@ -6,6 +6,10 @@ function Form() {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [firstNameIsValid, setFirstNameIsValid] = useState(true);
+    const [lastNameIsValid, setLastNameIsValid] = useState(true);
+    const [emailIsValid, setEmailIsValid] = useState(true);
+    const [passwordIsValid, setPasswordIsValid] = useState(true);
 
     const firstNameHandler = (event) => {
         setFirstName(event.target.value);
@@ -25,21 +29,72 @@ function Form() {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        console.log(firstName);
-        console.log(lastName);
-        console.log(email);
-        console.log(password);
+        if(firstName === "") {
+            setFirstNameIsValid(false);
+        }
+        else{
+            setFirstNameIsValid(true);
+        }
+
+        if(lastName === "") {
+            setLastNameIsValid(false);
+        }
+        else{
+            setLastNameIsValid(true);
+        }
+
+        if(email === "") {
+            setEmailIsValid(false);
+        }
+        else{
+            setEmailIsValid(true);
+        }
+
+        if(password === "") {
+            setPasswordIsValid(false);
+        }
+        else{
+            setPasswordIsValid(true);
+        }
+
+        if(firstName !== "" && lastName !== "" && email !== "" && password !== ""){
+            console.log(firstName);
+            console.log(lastName);
+            console.log(email);
+            console.log(password);
+
+            setFirstNameIsValid(true);
+            setLastNameIsValid(true);
+            setEmailIsValid(true);
+            setPasswordIsValid(true);
+        }
       };
 
     return (
         <form onSubmit={submitHandler}>
-            <input type="text" placeholder="First Name" onChange={firstNameHandler}></input>
+            <div className='InputField'>
+                <input type="text" placeholder="First Name" onChange={firstNameHandler} style={{ borderColor: firstNameIsValid ? null : "red" }}></input>
+                {firstNameIsValid ? null : <svg className="InvalidImage" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g></svg>}
+            </div>
+            {firstNameIsValid ? null : <p className='Invalid'>First name cannot be empty</p>}
             <br></br>
-            <input type="text" placeholder="Last Name" onChange={lastNameHandler}></input>
+            <div className='InputField'>
+                <input type="text" placeholder="Last Name" onChange={lastNameHandler} style={{ borderColor: lastNameIsValid ? null : "red" }}></input>
+                {lastNameIsValid ? null : <svg className="InvalidImage" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g></svg>}
+            </div>
+            {lastNameIsValid ? null : <p className='Invalid'>Last name cannot be empty</p>}
             <br></br>
-            <input type="text" placeholder="Email Address" onChange={emailHandler}></input>
+            <div className='InputField'>
+                <input type="text" placeholder="Email Address" onChange={emailHandler} style={{ borderColor: emailIsValid ? null : "red" }}></input>
+                {emailIsValid ? null : <svg className="InvalidImage" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g></svg>}
+            </div>
+            {emailIsValid ? null : <p className='Invalid'>Email cannot be empty</p>}
             <br></br>
-            <input type="text" placeholder="Password" onChange={passwordHandler}></input>
+            <div className='InputField'>
+                <input type="text" placeholder="Password" onChange={passwordHandler} style={{ borderColor: passwordIsValid ? null : "red" }}></input>
+                {passwordIsValid ? null : <svg className="InvalidImage" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g></svg>}
+            </div>
+            {passwordIsValid ? null : <p className='Invalid'>Password cannot be empty</p>}
             <br></br>
             <button className="Trial" type="submit">CLAIM YOUR FREE TRIAL</button>
         </form>
